b52e86bd0029d576a1f29cf0ad229b08d3a5901e
Jacque Pointer <jacquepointer1@gmail.com>
2022-10-17 14:45:23 -0600

    Formatted the document

--- CPRG311-Assignment-2/src/utilities/QueueADT.java
+++ CPRG311-Assignment-2/src/utilities/QueueADT.java
@@ -1,25 +1,24 @@
 package utilities;
 
-public interface QueueADT<E>{
-	
-	
+public interface QueueADT<E> {
+
 	public void enqueue(E toAdd);
-	
+
 	public E dequeue();
-	
+
 	public E peek();
-	
+
 	public boolean equals(QueueADT<E> that);
-	
+
 	public Iterator<E> iterator();
-	
+
 	public Object[] toArray();
-	
+
 	public E[] toArray(E[] copy);
-	
+
 	public boolean isFull();
-	
+
 	public int size();
-	
+
 	public void dequeueAll();
 }
