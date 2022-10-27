d447352b2a3799c12f7f156c83f7fcfc912ee65e
benmarcjayy <113211128+benmarcjayy@users.noreply.github.com>
2022-10-27 09:15:19 -0600

    Updated size, clear, add(int, E), add(E), remove(int), isEmpty

--- CPRG311-Assignment-2/src/utilities/MyDLL.java
+++ CPRG311-Assignment-2/src/utilities/MyDLL.java
@@ -7,21 +7,63 @@ public class MyDLL<E> implements ListADT<E>{
 	 */
 	private static final long serialVersionUID = -7140796753013938413L;
 
+	private class Node{
+		E element;
+		Node head;
+		Node tail;
+		
+		public Node(E element, Node head, Node tail){
+			this.element = element;
+			this.head = head;
+			this.tail = tail;
+		}
+	}
+
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		int res = 0;
+		while (node != null){
+			res++;
+			node = node.next;
+		}
+		return res;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		this.head = null;
+		this.tail = null;
 	}
 
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
+		if (index < 0 || index > numElements) 
+		throw new InvalidArgumentException();
+		Node<E> newNode = new Node<E>(element);
+		if (head == null) {	
+			head = newNode;
+			tail = newNode;
+		}
+		else if (index == 0) {
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+		}
+		else if (index == numElements) {
+			newNode.prev = tail;
+			tail.next = newNode;
+			tail = newNode;
+		}
+		else {	
+			Node<E> nodeRef = head;
+			for (int i = 1; i < index; i++) 
+				nodeRef = nodeRef.next;
+			newNode.next = nodeRef.next;
+			nodeRef.next = newNode;
+			newNode.prev = nodeRef;
+			newNode.next.prev = newNode;
+		}
+		numElements++;
 		return false;
 	}
 
@@ -39,14 +81,60 @@ public class MyDLL<E> implements ListADT<E>{
 
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		if (isEmpty()) {
+            System.out.println("List is empty");
+            return false;
+        }
+
+        Node curr = head;
+        while(curr != null && curr.data != key) {
+            curr = curr.next;
+        }
+
+        if (curr == null) {
+            return false;
+        }
+
+        return true;
 	}
 
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		 if (isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        // find the position of the key
+        Node curr = head;
+
+        while(curr != null && curr.data != key) {
+            curr = curr.next;
+        }
+
+        if(curr == null) {
+            System.out.print("key not found");
+            return;
+        }
+
+        if (curr.prev == null) {
+            popFront();
+
+        } else if (curr.next == null) { // if curr is last item
+            popBack();
+
+        } else { //anywhere between first and last node
+            Node next = curr.next;
+            Node prev = curr.prev;
+
+            prev.next = next;
+            next.prev = prev;
+
+            curr.prev = null;
+            curr.next = null;
+            curr = null;
+        }
+		return true;
 	}
 
 	@Override
@@ -63,8 +151,7 @@ public class MyDLL<E> implements ListADT<E>{
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return head == null;
 	}
 
 	@Override
